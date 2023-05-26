31b39b88200253a1a8c3f41e657bb9b178d05252
Aquila <ajmdietz@hotmail.com>
2023-05-26 13:00:26 -0300

    Carousel (com problemas)

--- App.tsx
+++ App.tsx
@@ -2,21 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Alterar from './app/screens/Alterar';
 import Lista from './app/screens/Lista';
 import Cadastro from './app/screens/Cadastro';
 import UploadFoto from './app/screens/UploadFoto';
+import CarouselCards from './app/screens/CarouselCards';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="UploadFoto" component={UploadFoto}/>
-        <Stack.Screen name="Cadastro" component={Cadastro}/>
-        <Stack.Screen name="Lista" component={Lista}/>
-        <Stack.Screen name="Alterar" component={Alterar}/>
+        <Stack.Screen name="CarouselCards" component={CarouselCards} />
+        <Stack.Screen name="UploadFoto" component={UploadFoto} />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
+        <Stack.Screen name="Lista" component={Lista} />
+        <Stack.Screen name="Alterar" component={Alterar} />
       </Stack.Navigator>
     </NavigationContainer>
   );
